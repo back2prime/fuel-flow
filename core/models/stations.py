fe623dcd0f67f2a37dc7892bbf597d73907642cd
back2prime <alextop7771@gmail.com>
2026-05-14 15:23:09 +0200

    chore: recreate migrations with length constraints

--- core/models/stations.py
+++ core/models/stations.py
@@ -1,10 +1,11 @@
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped, relationship,mapped_column
+from sqlalchemy import String
 
 from core.models.base import Base
 
 
 class Station(Base):
 
-    name: Mapped[str]
-    address: Mapped[str]
+    name: Mapped[str] = mapped_column(String(50))
+    address: Mapped[str] =  mapped_column(String(255))
     prices = relationship(argument="Price", back_populates="station")
