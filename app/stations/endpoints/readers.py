b8c64677699e20ca42acf7705225ad8c94f8c21a
back2prime <alextop7771@gmail.com>
2026-05-16 16:54:36 +0200

    refactor: rename router variable to stations_routers

--- app/stations/endpoints/readers.py
+++ app/stations/endpoints/readers.py
@@ -3,12 +3,10 @@ from fastapi import APIRouter
 from app.stations.schemes import StationsGetSchemes, StationsShowSchemes
 from app.services.tankerkoenig import get_stations
 
-stations_routers  = APIRouter()
+stations_routers = APIRouter()
 
 
 @stations_routers.post(path="/stations", response_model=list[StationsShowSchemes])
-async def get_best_stations(data: StationsGetSchemes,limit: int = None):
+async def get_best_stations(data: StationsGetSchemes, limit: int = None):
     res = await get_stations(data)
     return res[:limit] if limit else res
-
-
