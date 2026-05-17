c81ce80e934f405c71a45bba29691a2f122c1c7a
back2prime <alextop7771@gmail.com>
2026-05-17 22:07:05 +0200

    feat: add get station by id endpoint

--- app/stations/endpoints/readers.py
+++ app/stations/endpoints/readers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from app.stations.schemes import StationsGetSchemes, StationsShowSchemes
-from app.services.tankerkoenig import get_stations
+from app.services.tankerkoenig import get_stations, get_specific_station
+from app.stations.schemes.stations import StationShowInfo
 
 stations_routers = APIRouter()
 
@@ -10,3 +11,9 @@ stations_routers = APIRouter()
 async def get_best_stations(data: StationsGetSchemes, limit: int = None):
     res = await get_stations(data)
     return res[:limit] if limit else res
+
+
+@stations_routers.post(path="/stations/{id}", response_model=StationShowInfo)
+async def get_station(station_id: str):
+    res = await get_specific_station(station_id)
+    return res
