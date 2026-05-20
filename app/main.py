1b56f4b46de27fbd31331f7184c3c119a26adcad
back2prime <alextop7771@gmail.com>
2026-05-20 10:04:47 +0200

    feat: add redis_helper close to lifespan

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.stations.endpoints.readers import stations_routers as st_router
-from core.http_helper import http_helper
+from core.helpers.http_helper import http_helper
+from core.helpers.redis_helper import redis_helper
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
     await http_helper.close()
+    await redis_helper.close()
 
 
 app = FastAPI(lifespan=lifespan)
