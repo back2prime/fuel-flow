b335ddb34a11550d086d9489c208243d6c500d04
back2prime <alextop7771@gmail.com>
2026-05-27 21:23:21 +0200

    feat: add /users/get_user endpoint and user auth routes

--- app/users/endpoints/registration.py
+++ app/users/endpoints/registration.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.users import User
+from app.users import User, CurrentUser
 from app.users.schemes import UserGetScheme, UserRegisterScheme, UserLoginScheme
 
 from app.database.dependencies import SessionDep
@@ -28,3 +28,11 @@ async def register_user(data: UserRegisterScheme, db: SessionDep) -> User:
 async def login_user(data: UserLoginScheme, db: SessionDep) -> dict:
     token = await auth_user(data=data, session=db)
     return {"access_token": token, "token_type": "bearer"}
+
+@user_routers.get(
+    path="/users/me",
+    tags=["Users"],
+    response_model=UserGetScheme,
+)
+async def get_user(user:CurrentUser) -> User:
+    return user
