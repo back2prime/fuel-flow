30a58903711683a167b510a401794db9216cd468
back2prime <alextop7771@gmail.com>
2026-05-28 19:58:30 +0200

    rename: registration.py to users.py in endpoints

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.stations.endpoints.readers import stations_routers as st_router
-from app.users.endpoints.registration import user_routers as ur_router
+from app.users.endpoints.routers import user_routers as ur_router
 from core.helpers.http_helper import http_helper
 from core.helpers.redis_helper import redis_helper
 
