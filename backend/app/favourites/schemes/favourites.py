9c99297599ae63a8c187c1f0c77a46fd62130bd9
back2prime <alextop7771@gmail.com>
2026-06-07 17:25:11 +0200

    fix(backend): add station_id to FavouriteGetScheme

--- backend/app/favourites/schemes/favourites.py
+++ backend/app/favourites/schemes/favourites.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class FavouriteGetScheme(BaseModel):
+    station_id: str
     name: str
     address: str = Field(max_length=255)
     is_open: bool | None = Field(serialization_alias="isOpen")
