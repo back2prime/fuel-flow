d7ed19222c38000800eb4e996f0bd718b99bcd2a
back2prime <alextop7771@gmail.com>
2026-06-29 09:48:17 +0200

    feat(backend): add rate limiting on station endpoints

--- backend/app/stations/endpoints/routers.py
+++ backend/app/stations/endpoints/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query,Request
 
 from app.enums import ApiMethod, ResponseKey, CachePrefix
 from app.services.dependencies import TankerkoenigDep
@@ -9,7 +9,6 @@ from app.stations.schemes.stations import (
     StationShowInfo,
 )
 from core.helpers.limiter import limiter
-
 stations_routers = APIRouter()
 
 
@@ -18,6 +17,7 @@ stations_routers = APIRouter()
 )
 @limiter.limit("5/minute")
 async def get_best_stations(
+    request: Request,
     data: StationsGetSchemes,
     service: TankerkoenigDep,
     limit: int = Query(default=10, ge=1, le=50),
@@ -35,7 +35,7 @@ async def get_best_stations(
     path="/stations/{station_id}", tags=["Stations"], response_model=StationShowInfo
 )
 @limiter.limit("5/minute")
-async def get_station(station_id: str, service: TankerkoenigDep):
+async def get_station(request: Request,station_id: str, service: TankerkoenigDep):
     res = await service.get_redis_response(
         obj=StationGetScheme(id=station_id),
         prefix=CachePrefix.STATION,
