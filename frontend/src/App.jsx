63d0d391023543a5cd16be365821f87e8f55f1be
back2prime <alextop7771@gmail.com>
2026-06-30 10:55:51 +0200

    feat(frontend): sync isLoggedIn state on login/logout and initial auth check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import SearchPage from './pages/SearchPage'
 import LoginPage from './pages/LoginPage'
@@ -7,14 +8,27 @@ import FavouritesPage from './pages/FavouritesPage'
 import ProfilePage from './pages/ProfilePage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
+import client from './api/client'
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [authLoading, setAuthLoading] = useState(true)
+
+  useEffect(() => {
+    client.get('/users/me')
+      .then(() => setIsLoggedIn(true))
+      .catch(() => setIsLoggedIn(false))
+      .finally(() => setAuthLoading(false))
+  }, [])
+
+  if (authLoading) return null
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
         <Route path="/" element={<SearchPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/favourites" element={<FavouritesPage />} />
         <Route path="/profile" element={<ProfilePage />} />
